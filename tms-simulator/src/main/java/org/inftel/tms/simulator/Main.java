02d176f7b1de1efbf65eaa4ef119fa102d71bd1b
ignacio.bacamt@gmail.com <ignacio.bacamt@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-22 00:55:19 +0000

    modulos del sistema tms (Telecare Management System)
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@2 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

---
+++ tms-simulator/src/main/java/org/inftel/tms/simulator/Main.java
@@ -0,0 +1,12 @@
+package org.inftel.tms.simulator;
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
