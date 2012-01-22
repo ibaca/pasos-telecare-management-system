a1f0c625bae3dd6bbe3542db9c6d858bc9a0aea0
ignacio.bacamt@gmail.com <ignacio.bacamt@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-22 16:38:02 +0000

    mejorado ejemplo tms-simulator para ver como se configura el host de CORBA
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@14 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-simulator/src/main/java/org/inftel/tms/simulator/Main.java
+++ tms-simulator/src/main/java/org/inftel/tms/simulator/Main.java
@@ -1,5 +1,6 @@
 package org.inftel.tms.simulator;
 
+import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import org.inftel.tms.services.UserFacadeRemote;
@@ -20,8 +21,19 @@ public class Main {
   public static final String USER_FACADE_JNDI = "java:global/org.inftel.tms_tms-bundle_ear_1.0-SNAPSHOT/tms-core-1.0-SNAPSHOT/UserFacade!org.inftel.tms.services.UserFacadeRemote";
 
   public static void main(String[] args) throws Exception {
-    Context context = new InitialContext();
     System.out.println("Hello World Enterprise Application Client!");
+
+    // Configuraciones, ahora no hace falta porque por defecto busca en localhost! pero cuando
+    // se usa en diferentes maquinas el host debe ser un parametro configurable
+    // mas info en: http://docs.oracle.com/cd/E19651-01/817-2150-10/dccorba.html
+    Properties env = new Properties();
+    env.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+    //env.setProperty("java.naming.provider.url", "iiop://hostname:port");
+
+    // Se ininializa el contexto apuntando a nuestro servidor glassfish
+    Context context = new InitialContext(env);
+
+    // Se llama a algun metodo para ver que todo va bien!
     UserFacadeRemote userService = (UserFacadeRemote) context.lookup(USER_FACADE_JNDI);
     System.out.println("remote user service count response: " + userService.count());
   }
