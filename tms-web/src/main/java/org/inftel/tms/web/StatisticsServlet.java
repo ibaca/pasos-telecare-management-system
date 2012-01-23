ee5e48daaa69d3f7b6d67216c46944303e9b5ead
agumpg@gmail.com <agumpg@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-23 23:24:28 +0000

    Prueba inicial para grafica por perfiles de usuarios
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@36 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

---
+++ tms-web/src/main/java/org/inftel/tms/web/StatisticsServlet.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inftel.tms.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.inftel.tms.statistics.ChartUserTypeBean;
+
+/**
+ *
+ * @author agumpg
+ */
+@WebServlet(name = "StatisticsServlet", urlPatterns = {"/StatisticsServlet"})
+public class StatisticsServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String nextJSP = "/statistics.jsp";
+        String action = request.getParameter("action");
+
+        if (action.equals("Statistic_UserType")) {
+            nextJSP = "/statistics/graphicUserType.jsp";
+            
+            ChartUserTypeBean chartUserTypeBean = new ChartUserTypeBean();
+            request.setAttribute("chartUserTypeBean", chartUserTypeBean);        
+        }
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+        dispatcher.forward(request,response);
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ tms-web/src/main/webapp/statistics.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : statistics
+    Created on : 23-ene-2012, 23:00:31
+    Author     : agumpg
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Estadísticas</title>
+    </head>
+    <body>
+        <h1>Estadísticas</h1>
+        <a href="${pageContext.request.contextPath}/StatisticsServlet?action=Statistic_UserType">Tipos de Usuarios</a>
+    </body>
+</html>
