1cb2ff9065e6d8089f0cc35a68f848d97ac2628a
ignacio.bacamt@gmail.com <ignacio.bacamt@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-24 00:55:14 +0000

    movido ChartUsertType dentro de StaticServlet
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@37 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-web/src/main/java/org/inftel/tms/web/StatisticsServlet.java
+++ tms-web/src/main/java/org/inftel/tms/web/StatisticsServlet.java
@@ -5,15 +5,14 @@
 package org.inftel.tms.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import java.io.Serializable;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.inftel.tms.statistics.ChartUserTypeBean;
+import org.primefaces.model.chart.PieChartModel;
 
 /**
  *
@@ -22,73 +21,99 @@ import org.inftel.tms.statistics.ChartUserTypeBean;
 @WebServlet(name = "StatisticsServlet", urlPatterns = {"/StatisticsServlet"})
 public class StatisticsServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        String nextJSP = "/statistics.jsp";
-        String action = request.getParameter("action");
+  public static class ChartUserTypeBean implements Serializable {
 
-        if (action.equals("Statistic_UserType")) {
-            nextJSP = "/statistics/graphicUserType.jsp";
-            
-            ChartUserTypeBean chartUserTypeBean = new ChartUserTypeBean();
-            request.setAttribute("chartUserTypeBean", chartUserTypeBean);        
-        }
+    private PieChartModel pieModel;
 
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
-        dispatcher.forward(request,response);
-        
-        
+    public void ChartBean() {
+      createPieModel();
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public PieChartModel getPieModel() {
+      return pieModel;
     }
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    private void createPieModel() {
+      pieModel = new PieChartModel();
+
+
+      pieModel.set("Mayor de 65 años", 45);
+      pieModel.set("Discapacidad Física", 7);
+      pieModel.set("Discapacidad Psíquica", 1);
+      pieModel.set("Discapacidad Sensorial", 2);
+      pieModel.set("Enfermos crónicos", 4);
+      pieModel.set("Violencia de Género", 17);
+      pieModel.set("Otros", 24);
+    }
+  }
+
+  /**
+   * Processes requests for both HTTP
+   * <code>GET</code> and
+   * <code>POST</code> methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+    String nextJSP = "/statistics.jsp";
+    String action = request.getParameter("action");
+
+    if (action.equals("Statistic_UserType")) {
+      nextJSP = "/statistics/graphicUserType.jsp";
+
+      ChartUserTypeBean chartUserTypeBean = new ChartUserTypeBean();
+      request.setAttribute("chartUserTypeBean", chartUserTypeBean);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+    dispatcher.forward(request, response);
+
+
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP
+   * <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP
+   * <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
 }
