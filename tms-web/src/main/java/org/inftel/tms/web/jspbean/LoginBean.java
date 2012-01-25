2d2b915ca69ac8fa74c3c2b6414a6e497e6c4c92
miwe08@gmail.com <miwe08@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-25 18:29:00 +0000

    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@72 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-web/src/main/java/org/inftel/tms/web/jspbean/LoginBean.java
+++ tms-web/src/main/java/org/inftel/tms/web/jspbean/LoginBean.java
@@ -4,18 +4,24 @@
  */
 package org.inftel.tms.web.jspbean;
 
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import org.inftel.tms.domain.Alert;
+import org.inftel.tms.services.UserFacadeRemote;
 import org.primefaces.context.RequestContext;
 
 /**
  *
  * @author Administrador
  */
-public class LoginBean {  
+public class LoginBean {
+    @EJB
+    private UserFacadeRemote userFacade;
   
     private String username; 
     private String password;
+    private boolean loggedIn=false;
       
     public String getUsername() {  
         return username;  
@@ -35,9 +41,7 @@ public class LoginBean {
   
     public void login() { 
         RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg;  
-        boolean loggedIn;  
-          
+        FacesMessage msg;
         if(username != null  && username.equals("admin") && password != null  && password.equals("admin")) {  
             loggedIn = true;  
             msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
@@ -48,5 +52,13 @@ public class LoginBean {
         } 
         FacesContext.getCurrentInstance().addMessage(null, msg);  
         context.addCallbackParam("loggedIn", loggedIn);  
-    }  
+    }
+    
+//    public String goTo(){
+//        if(loggedIn){
+//            return "success-login";
+//        }else{
+//            return "bad-login";
+//        }
+//    }
 }  
