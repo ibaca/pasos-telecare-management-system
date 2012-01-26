3452560215add1969d1ac06336eac2078c49aa92
miwe08@gmail.com <miwe08@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-26 11:43:15 +0000

    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@85 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-web/src/main/java/org/inftel/tms/web/jspbean/TableBean.java
+++ tms-web/src/main/java/org/inftel/tms/web/jspbean/TableBean.java
@@ -9,85 +9,99 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
+import javax.ejb.EJB;
+import org.inftel.tms.domain.Alert;
+import org.inftel.tms.services.AlertFacadeRemote;
 
 /**
  *
  * @author inftel
  */
-public class TableBean implements Serializable {  
+public class TableBean implements Serializable {
+    @EJB
+   static private AlertFacadeRemote alertFacade;
       
-    private final static Logger logger = Logger.getLogger(TableBean.class.getName());  
-      
-    private final static String[] colors;  
-      
-    private final static String[] manufacturers;  
-      
-    static {  
-        colors = new String[10];  
-        colors[0] = "Black";  
-        colors[1] = "White";  
-        colors[2] = "Green";  
-        colors[3] = "Red";  
-        colors[4] = "Blue";  
-        colors[5] = "Orange";  
-        colors[6] = "Silver";  
-        colors[7] = "Yellow";  
-        colors[8] = "Brown";  
-        colors[9] = "Maroon";  
-          
-        manufacturers = new String[10];  
-        manufacturers[0] = "Mercedes";  
-        manufacturers[1] = "BMW";  
-        manufacturers[2] = "Volvo";  
-        manufacturers[3] = "Audi";  
-        manufacturers[4] = "Renault";  
-        manufacturers[5] = "Opel";  
-        manufacturers[6] = "Volkswagen";  
-        manufacturers[7] = "Chrysler";  
-        manufacturers[8] = "Ferrari";  
-        manufacturers[9] = "Ford";  
-    }  
+//    private final static Logger logger = Logger.getLogger(TableBean.class.getName());  
+//      
+//    private final static String[] colors;  
+//      
+//    private final static String[] manufacturers;  
+//      
+//    static {  
+//        colors = new String[10];  
+//        colors[0] = "Black";  
+//        colors[1] = "White";  
+//        colors[2] = "Green";  
+//        colors[3] = "Red";  
+//        colors[4] = "Blue";  
+//        colors[5] = "Orange";  
+//        colors[6] = "Silver";  
+//        colors[7] = "Yellow";  
+//        colors[8] = "Brown";  
+//        colors[9] = "Maroon";  
+//          
+//        manufacturers = new String[10];  
+//        manufacturers[0] = "Mercedes";  
+//        manufacturers[1] = "BMW";  
+//        manufacturers[2] = "Volvo";  
+//        manufacturers[3] = "Audi";  
+//        manufacturers[4] = "Renault";  
+//        manufacturers[5] = "Opel";  
+//        manufacturers[6] = "Volkswagen";  
+//        manufacturers[7] = "Chrysler";  
+//        manufacturers[8] = "Ferrari";  
+//        manufacturers[9] = "Ford";  
+//    }  
   
-    private List<Car> cars;  
+    private List<Alert> alerts;  
       
-    private Car selectedCar;  
+    private Alert selectedAlert;  
   
-    public TableBean() {  
-        cars = new ArrayList<Car>();  
-        populateRandomCars(cars, 50); 
+    public TableBean() {
+        alerts=alertFacade.findActiveAlerts();
     }  
-      
-    public Car getSelectedCar() {  
-        return selectedCar;  
-    }  
+    
+//    private void populateRandomCars(List<Car> list, int size) {  
+//        for(int i = 0 ; i < size ; i++)  
+//            list.add(new Car(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));  
+//    }  
   
-    public void setSelectedCar(Car selectedCar) {  
-        this.selectedCar = selectedCar;  
-    }  
+//    private int getRandomYear() {  
+//        return (int) (Math.random() * 50 + 1960);  
+//    }  
+//      
+//    private String getRandomColor() {  
+//        return colors[(int) (Math.random() * 10)];  
+//    }  
+//      
+//    private String getRandomManufacturer() {  
+//        return manufacturers[(int) (Math.random() * 10)];  
+//    }  
+//      
+//    private String getRandomModel() {  
+//        return UUID.randomUUID().toString().substring(0, 8);  
+//    }    
 
-  
-    private void populateRandomCars(List<Car> list, int size) {  
-        for(int i = 0 ; i < size ; i++)  
-            list.add(new Car(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));  
-    }  
-  
-    public List<Car> getCars() {
-        return cars;  
-    }  
-  
-    private int getRandomYear() {  
-        return (int) (Math.random() * 50 + 1960);  
-    }  
-      
-    private String getRandomColor() {  
-        return colors[(int) (Math.random() * 10)];  
-    }  
-      
-    private String getRandomManufacturer() {  
-        return manufacturers[(int) (Math.random() * 10)];  
-    }  
-      
-    private String getRandomModel() {  
-        return UUID.randomUUID().toString().substring(0, 8);  
-    }    
+    public List<Alert> getAlerts() {
+        return alerts;
+    }
+
+    public void setAlerts(List<Alert> alerts) {
+        this.alerts = alerts;
+    }
+
+    public Alert getSelectedAlert() {
+        return selectedAlert;
+    }
+
+    public void setSelectedAlert(Alert selectedAlert) {
+        this.selectedAlert = selectedAlert;
+    }
+    
+    public static void main(String[] args){
+        List<Alert> talerts = alertFacade.findActiveAlerts();
+        for(Alert i : talerts){
+            System.out.println(i.getAffected().getFirstName());
+        }
+    }
 } 
