b2e0037e50bf115538db75f9c1a69637c23cc056
ignacio.bacamt@gmail.com <ignacio.bacamt@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-26 18:51:50 +0000

    metodo para obtener dispositivo apartir del numero de movil
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@97 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-api/src/main/java/org/inftel/tms/domain/Device.java
+++ tms-api/src/main/java/org/inftel/tms/domain/Device.java
@@ -7,80 +7,83 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * Representa a los dispositivos asiciados al sistema. Cada afectado debe tener almenos un
  * dispositivo activo. Los dispositivos deben delegar las posiciones recibidas al afectado.
- *
+ * 
  * @author ibaca
  */
 @Entity
 @Table(name = "devices")
 @XmlRootElement
 @NamedQueries({
-  @NamedQuery(name = "Device.findAll", query = "SELECT d FROM Device d"),
-  @NamedQuery(name = "Device.findById", query = "SELECT d FROM Device d WHERE d.id = :id"),
-  @NamedQuery(name = "Device.findByBatery", query = "SELECT d FROM Device d WHERE d.batery = :batery"),
-  @NamedQuery(name = "Device.findByCreated", query = "SELECT d FROM Device d WHERE d.created = :created"),
-  @NamedQuery(name = "Device.findByLastconnection", query = "SELECT d FROM Device d WHERE d.lastConnection = :lastconnection"),
-  @NamedQuery(name = "Device.findByMobilenumber", query = "SELECT d FROM Device d WHERE d.mobileNumber = :mobilenumber"),
-  @NamedQuery(name = "Device.findBySimcard", query = "SELECT d FROM Device d WHERE d.simCard = :simcard"),
-  @NamedQuery(name = "Device.findByTemperature", query = "SELECT d FROM Device d WHERE d.temperature = :temperature"),
-  @NamedQuery(name = "Device.findByUpdated", query = "SELECT d FROM Device d WHERE d.updated = :updated"),
-  @NamedQuery(name = "Device.findByVersion", query = "SELECT d FROM Device d WHERE d.version = :version")})
+        @NamedQuery(name = "Device.findAll", query = "SELECT d FROM Device d"),
+        @NamedQuery(name = "Device.findById", query = "SELECT d FROM Device d WHERE d.id = :id"),
+        @NamedQuery(name = "Device.findByBatery", query = "SELECT d FROM Device d WHERE d.batery = :batery"),
+        @NamedQuery(name = "Device.findByCreated", query = "SELECT d FROM Device d WHERE d.created = :created"),
+        @NamedQuery(name = "Device.findByLastconnection", query = "SELECT d FROM Device d WHERE d.lastConnection = :lastconnection"),
+        @NamedQuery(name = Device.FIND_BY_MOBILE, query = "SELECT d FROM Device d WHERE d.mobileNumber = :mobile"),
+        @NamedQuery(name = "Device.findBySimcard", query = "SELECT d FROM Device d WHERE d.simCard = :simcard"),
+        @NamedQuery(name = "Device.findByTemperature", query = "SELECT d FROM Device d WHERE d.temperature = :temperature"),
+        @NamedQuery(name = "Device.findByUpdated", query = "SELECT d FROM Device d WHERE d.updated = :updated"),
+        @NamedQuery(name = "Device.findByVersion", query = "SELECT d FROM Device d WHERE d.version = :version") })
 public class Device extends BaseEntity {
 
-  private String mobileNumber;
-  private String simCard;
-  private Integer batery;
-  private Integer temperature;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date lastConnection;
-  @ManyToOne
-  @JoinColumn(nullable = true) // aunq no es lo normal, puede ser nulo!
-  private Affected owner;
+    public final static String FIND_BY_MOBILE = "Device.findByMobilenumber";
+    
+    private String mobileNumber;
+    private String simCard;
+    private Integer batery;
+    private Integer temperature;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastConnection;
+    @ManyToOne
+    @JoinColumn(nullable = true)
+    // aunq no es lo normal, puede ser nulo!
+    private Affected owner;
 
-  public Date getLastConnection() {
-    return lastConnection;
-  }
+    public Date getLastConnection() {
+        return lastConnection;
+    }
 
-  public void setLastConnection(Date lastConnection) {
-    this.lastConnection = lastConnection;
-  }
+    public void setLastConnection(Date lastConnection) {
+        this.lastConnection = lastConnection;
+    }
 
-  public Integer getTemperature() {
-    return temperature;
-  }
+    public Integer getTemperature() {
+        return temperature;
+    }
 
-  public void setTemperature(Integer temperature) {
-    this.temperature = temperature;
-  }
+    public void setTemperature(Integer temperature) {
+        this.temperature = temperature;
+    }
 
-  public Affected getOwner() {
-    return owner;
-  }
+    public Affected getOwner() {
+        return owner;
+    }
 
-  public void setOwner(Affected owner) {
-    this.owner = owner;
-  }
+    public void setOwner(Affected owner) {
+        this.owner = owner;
+    }
 
-  public Integer getBatery() {
-    return batery;
-  }
+    public Integer getBatery() {
+        return batery;
+    }
 
-  public void setBatery(Integer batery) {
-    this.batery = batery;
-  }
+    public void setBatery(Integer batery) {
+        this.batery = batery;
+    }
 
-  public String getMobileNumber() {
-    return mobileNumber;
-  }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
 
-  public void setMobileNumber(String mobileNumber) {
-    this.mobileNumber = mobileNumber;
-  }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 
-  public String getSimCard() {
-    return simCard;
-  }
+    public String getSimCard() {
+        return simCard;
+    }
 
-  public void setSimCard(String simCard) {
-    this.simCard = simCard;
-  }
+    public void setSimCard(String simCard) {
+        this.simCard = simCard;
+    }
 }
--- tms-core/src/main/java/org/inftel/tms/services/DeviceFacade.java
+++ tms-core/src/main/java/org/inftel/tms/services/DeviceFacade.java
@@ -1,26 +1,36 @@
 package org.inftel.tms.services;
 
+import static org.inftel.tms.domain.Device.FIND_BY_MOBILE;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.inftel.tms.domain.Device;
 
 /**
- *
+ * 
  * @author ibaca
  */
 @Stateless
 public class DeviceFacade extends AbstractFacade<Device> implements DeviceFacadeRemote {
 
-  @PersistenceContext(unitName = "tms-persistence")
-  private EntityManager em;
+    @PersistenceContext(unitName = "tms-persistence")
+    private EntityManager em;
 
-  @Override
-  protected EntityManager getEntityManager() {
-    return em;
-  }
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
 
-  public DeviceFacade() {
-    super(Device.class);
-  }
+    public DeviceFacade() {
+        super(Device.class);
+    }
+
+    public Device findByMobile(String mobile) {
+        TypedQuery<Device> query = em.createNamedQuery(FIND_BY_MOBILE, Device.class);
+        query.setParameter("mobile", mobile);
+        return query.getSingleResult();
+    }
 }
