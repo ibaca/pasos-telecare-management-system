0a4264551aa709127e0abc3405729f798c0c21e1
cx_jimen17@hotmail.com <cx_jimen17@hotmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-27 13:50:54 +0000

    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@131 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-web/src/main/java/org/inftel/tms/web/jsfbean/UserWizard.java
+++ tms-web/src/main/java/org/inftel/tms/web/jsfbean/UserWizard.java
@@ -4,49 +4,71 @@
  */
 package org.inftel.tms.web.jsfbean;
 
-/**
- *
- * @author inftel
- */
 import java.awt.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import org.primefaces.event.FlowEvent;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.inftel.tms.domain.Device;
+import org.inftel.tms.domain.Person;
+import org.inftel.tms.domain.User;
+import org.inftel.tms.services.DeviceFacadeRemote;
+import org.inftel.tms.services.PeopleFacadeRemote;
 
 /**
  *
  * @author inftel
  */
-public class UserWizard {  
-  
-   
-      
-    private boolean skip;  
-  
+/**
+ *
+ * @author inftel
+ */
+public class UserWizard {
 
-      
-    public void save(ActionEvent actionEvent) {  
-        //Persist user  
-          
-        FacesMessage msg = new FacesMessage("Successful", "Welcome :");  
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-    }  
-      
-    public boolean isSkip() {  
-        return skip;  
-    }  
-  
-    public void setSkip(boolean skip) {  
-        this.skip = skip;  
-    }  
-      
-    public String onFlowProcess(FlowEvent event) {  
-        if(skip) {  
-            skip = false;   //reset in case user goes back  
-            return "confirm";  
-        }  
-        else {  
-            return event.getNewStep();  
-        }  
-    }  
-}  
+    DeviceFacadeRemote deviceFacade = lookupDeviceFacadeRemote();
+    PeopleFacadeRemote peopleFacade = lookupPeopleFacadeRemote();
+    Person p;
+    Device d;
+    private String Firstname;
+    private String Lastname;
+    private String email;
+    private String Mobilenumber;
+
+    public void save (){
+        //Inicialiamos la persona
+        p= new Person();
+        p.setFirstName(Firstname);
+        p.setLastName(Lastname);
+        p.setEmail(email);
+        p.setMobileNumber(Mobilenumber);
+        //Inicializamos el dispositivo
+        d=new Device();
+        d.setMobileNumber(Mobilenumber);
+        //Creamos los dos nuevos objetos
+        peopleFacade.create(p);
+        deviceFacade.create(d);
+    }
+
+    private PeopleFacadeRemote lookupPeopleFacadeRemote() {
+        try {
+            Context c = new InitialContext();
+            return (PeopleFacadeRemote) c.lookup("java:global/org.inftel.tms_tms-bundle_ear_1.0-SNAPSHOT/org.inftel.tms_tms-core_ejb_1.0-SNAPSHOT/PeopleFacade!org.inftel.tms.services.PeopleFacadeRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+    private DeviceFacadeRemote lookupDeviceFacadeRemote() {
+        try {
+            Context c = new InitialContext();
+            return (DeviceFacadeRemote) c.lookup("java:global/org.inftel.tms_tms-bundle_ear_1.0-SNAPSHOT/org.inftel.tms_tms-core_ejb_1.0-SNAPSHOT/DeviceFacade!org.inftel.tms.services.DeviceFacadeRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
