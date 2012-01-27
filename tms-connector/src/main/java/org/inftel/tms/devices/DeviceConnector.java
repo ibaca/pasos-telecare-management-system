f88d3023576697551323f6869df6cd2cd474f20b
migueqm@gmail.com <migueqm@gmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-01-27 11:13:13 +0000

    Añadida posibilidad de seleccionar localizacion en la aplicacion SWING
    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@122 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

--- tms-connector/src/main/java/org/inftel/tms/devices/DeviceConnector.java
+++ tms-connector/src/main/java/org/inftel/tms/devices/DeviceConnector.java
@@ -35,7 +35,6 @@ import org.inftel.tms.services.DeviceFacadeRemote;
 public class DeviceConnector implements DeviceConnectorRemote {
   @EJB
   private DeviceFacadeRemote deviceFacade;
-
   @EJB
   private AlertFacadeRemote alertFacade;
   @EJB
@@ -210,6 +209,7 @@ public class DeviceConnector implements DeviceConnectorRemote {
    * @param raw
    * @param mobileNumber
    */
+    @Override
   public void createAlert(AlertType type, AlertPriority priority,String cause, AlertRaw raw,String mobileNumber){
       Alert alert = new Alert();
       Device device = deviceFacade.findByMobile(mobileNumber);
