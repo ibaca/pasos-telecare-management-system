acc7ae7fc375b08b732eeff3964b40676e384ca9
cx_jimen17@hotmail.com <cx_jimen17@hotmail.com@a03e1d51-c13a-4d2a-65c6-6c5e8594b909>
2012-03-04 18:00:55 +0000

    git-svn-id: http://proyecto-javaee.googlecode.com/svn/trunk@209 a03e1d51-c13a-4d2a-65c6-6c5e8594b909

---
+++ tms-mobile/src/org/inftel/tms/mobile/userAlarmaInterface/sendAU.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.inftel.tms.mobile.userAlarmaInterface;
+
+import org.inftel.tms.mobile.R;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * @author Cristian
+ *
+ */
+public class sendAU extends Activity {
+    /** Called when the activity is first created. */
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sending); 
+        
+        //Recuperar los datos a�adidos al intent padre
+        Bundle userdata = getIntent().getBundleExtra("USERDATA");
+        Bundle serverdata = getIntent().getBundleExtra("SERVERDATA");
+        
+        //Sacar algun dato por pantalla (opcional)
+        TextView mssg = (TextView) findViewById(R.id.mssgSending);
+        mssg.setText("Hola "+userdata.get("NAME").toString()+" envio tus datos a "+serverdata.get("URI").toString());
+        
+        //Enviando los datos de Ususario a traves de la API de MigueQ
+        //con los datos recuperados del intent padre
+        
+    }
+}
